c56a530ce08ace35a11e4c5bbc742678f1a2bb1e
rlaqhalx <rlaqhalx@gmail.com>
2022-10-26 23:46:11 -0400

    minor change to lightmodebutton

--- src/components/LightModeButton.jsx
+++ src/components/LightModeButton.jsx
@@ -4,9 +4,15 @@ import { IconSun, IconMoonStars } from '@tabler/icons';
 function LightModeButton() {
 
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const dark = colorScheme ===  'dark';
   
   return (
-    <ActionIcon variant="default" onClick={() => toggleColorScheme()} size={30}>
+    <ActionIcon 
+      variant="default" 
+      color={dark ? "yellow" : "blue"}
+      onClick={() => toggleColorScheme()} 
+      size={30}
+    >
       {colorScheme === "dark" ? (
         <IconSun size={16} />
       ) : (
